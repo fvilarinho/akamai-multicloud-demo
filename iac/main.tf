497191a64ad3d923bb0e6ac4814de046ac4f1e25
fvilarin <fvilarin@akamai.com>
2024-05-16 11:50:21 -0300

    Code improvements and bug fixes.

--- iac/main.tf
+++ iac/main.tf
@@ -7,6 +7,8 @@ terraform {
     region                      = "us-east-1"
     endpoint                    = "us-east-1.linodeobjects.com"
     skip_credentials_validation = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
   }
 
   # Required providers.
